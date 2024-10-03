8f63cac872b698e9d832af69e76f12da3eb338c1
vinaysu <suddakuntivinay7877@gmail.com>
2024-10-03 23:17:59 +0530

    mongoose connect

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
