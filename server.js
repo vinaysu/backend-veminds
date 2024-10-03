d3538c32923dbb043c92c833e455da9720b91bce
vinaysu <suddakuntivinay7877@gmail.com>
2024-10-03 22:28:14 +0530

    cors added

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: '*', // You can restrict this to your frontend URL
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
 app.use(express.json());
 
 // Connect to MongoDB
