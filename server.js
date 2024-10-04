eea5280d84a0bdce702ce0ece8cdaf46ce59cc74
vinaysu <suddakuntivinay7877@gmail.com>
2024-10-04 08:23:07 +0530

    timeout

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000 // 30 seconds timeout
+})
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
